18a573591e16fd893f324c76b83be610c7ba16cd
Ken Snyder <ken@ken.net>
2020-07-22 15:08:29 -0700

    remove remaining dist files

--- dist/@types/general.d.ts
+++
@@ -1,7 +0,0 @@
-export interface ISecurityRule {
-    read?: string | boolean;
-    write?: string | boolean;
-    validate?: string | boolean;
-    query?: string;
-    indexOn?: string | string[];
-}
